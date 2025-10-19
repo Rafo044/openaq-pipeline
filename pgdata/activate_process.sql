809b9095fdb0d7fa1fcaf15b9fd2884255db0299
Rafo044 <rafo044@protonmail.com>
2025-10-19 15:13:16 +0400

    Change activate_log.sql to activate_process.sql for add backup strategy

--- pgdata/activate_process.sql
+++ pgdata/activate_process.sql
@@ -2,4 +2,9 @@ ALTER SYSTEM SET logging_collector = on;
 ALTER SYSTEM SET log_directory = 'pg_log';
 ALTER SYSTEM SET log_filename = 'postgresql-%Y-%m-%d_%H%M%S.log';
 ALTER SYSTEM SET log_statement = 'all';
+ALTER SYSTEM SET archive_mode = 'ON';
+
+ALTER SYSTEM SET archive_command = 'pgbackrest --stanza=main archive-push %p'
+ALTER SYSTEM SET restore_command = 'pgbackrest --stanza=main archive-get %f %p'
+
 SELECT pg_reload_conf();
